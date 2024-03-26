1981a7b356a9a250af000f96dd3e3501f2ab0cdd
MWesolowski <Maciej.Wesolowski@ncbj.gov.pl>
2024-03-26 11:06:45 +0100

    DIsplay

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Git handson")
+print("Git_handson")
+DATA = [
+    {
+        "imię": "Burek",
+        "gatunek": "Kot",
+        "waga": 3,
+        "wiek": 4
+    },
+    {
+        "imię": "Bruno",
+        "gatunek": "Pies",
+        "waga": 35,
+        "wiek": 3
+    }
+]
+
+def display(data):
+    print(data)
+   
+display(DATA)
